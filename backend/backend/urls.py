3a810f9c2db6adc6eec1fe76485e26b6baee35ac
user <kola89611@gmail.com>
2023-06-29 23:27:06 +0300

    Views, templates, urls for tickets

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounting/', include('accounting.urls')),
-    path('', include('user.urls')),
+    path('', include('accounting.urls')),
+    path('user/', include('user.urls')),
 ]
