4fd36c78b91fe4a249e2942b86b353ffa269092e
user <kola89611@gmail.com>
2023-07-03 23:50:43 +0300

    Added filters

--- backend/accounting/views.py
+++ backend/accounting/views.py
@@ -5,6 +5,7 @@ from django.urls import reverse
 from .forms import TicketForm
 from .models import Ticket
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Q
 
 
 @login_required
@@ -14,6 +15,62 @@ def view_tickets(request):
     page = request.GET.get('page', 1)
     paginator = Paginator(ticket_item, 10)
 
+    try:
+        tickets = paginator.page(page)
+    except PageNotAnInteger:
+        tickets = paginator.page(1)
+    except EmptyPage:
+        tickets = paginator.page(paginator.num_pages)
+
+    context = {
+        'tickets': tickets,
+        'tickets_quantity': tickets_quantity,
+        'title': 'Tickets',
+    }
+    return render(request, 'accounting/index.html', context=context)
+
+
+@login_required
+def view_filtered_tickets(request, key=None):
+    q = Q(user=request.user)
+    if key == 'waiting':
+        q = q & Q(profit=None)
+
+    if key == 'failure':
+        q = q & Q(profit__lt=0)
+
+    if key == 'nothing':
+        q = q & Q(profit=0)
+
+    if key == 'success':
+        q = q & Q(profit__gt=0)
+
+    ticket_item = Ticket.objects.filter(q)
+
+    if key == 'bought_highest_to_lowest':
+        ticket_item = ticket_item.order_by('-bought').values()
+
+    if key == 'bought_lowest_to_highest':
+        ticket_item = ticket_item.order_by('bought').values()
+
+    if key == 'sold_highest_to_lowest':
+        ticket_item = ticket_item.order_by('-sold').values()
+
+    if key == 'sold_lowest_to_highest':
+        ticket_item = ticket_item.order_by('sold').values()
+
+    if key == 'profit_highest_to_lowest':
+        ticket_item = ticket_item.order_by('-profit').values()
+
+    if key == 'profit_lowest_to_highest':
+        ticket_item = ticket_item.order_by('profit').values()
+
+    if key == 'date_oldest':
+        ticket_item = ticket_item.order_by('created_at').values()
+
+    tickets_quantity = ticket_item.count()
+    page = request.GET.get('page', 1)
+    paginator = Paginator(ticket_item, 10)
     try:
         tickets = paginator.page(page)
     except PageNotAnInteger:
