9d79456cbacfbd29fb6e904eabe8bd43d983a1f4
user <kola89611@gmail.com>
2023-07-07 20:21:00 +0300

    User Data

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='logout'),
     path('password_change/', password_change, name='password_change'),
-    path('account_profile/', account_profile, name='account_profile'),
-    path('user_data/', user_data, name='user_data'),
+    path('account_profile/', update_user_data, name='account_profile'),
 ]
