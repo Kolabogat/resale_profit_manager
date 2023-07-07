5faea3407f28dce4f5e12a49ffddd81755923573
user <kola89611@gmail.com>
2023-07-07 19:45:07 +0300

    Rework view_tickets function

--- backend/accounting/views.py
+++ backend/accounting/views.py
@@ -8,84 +8,66 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models import Q
 
 FILTER_BY = {
-    'waiting': 'waiting',
-    'failure': 'failure',
-    'nothing': 'nothing',
-    'success': 'success',
-    'bought_highest_to_lowest': 'bought_highest_to_lowest',
-    'bought_lowest_to_highest': 'bought_lowest_to_highest',
-    'sold_highest_to_lowest': 'sold_highest_to_lowest',
-    'sold_lowest_to_highest': 'sold_lowest_to_highest',
-    'profit_highest_to_lowest': 'profit_highest_to_lowest',
-    'profit_lowest_to_highest': 'profit_lowest_to_highest',
+    'waiting': 'profit_waiting',
+    'failure': 'profit_failure',
+    'nothing': 'profit_nothing',
+    'success': 'profit_success',
+    'bought_hl': 'bought_highest_to_lowest',
+    'bought_lh': 'bought_lowest_to_highest',
+    'sold_hl': 'sold_highest_to_lowest',
+    'sold_lh': 'sold_lowest_to_highest',
+    'profit_hl': 'profit_highest_to_lowest',
+    'profit_lh': 'profit_lowest_to_highest',
     'date_oldest': 'date_oldest',
 }
 
 
 @login_required
-def view_tickets(request):
-    ticket_item = Ticket.objects.filter(user=request.user)
-    tickets_quantity = ticket_item.count()
-    page = request.GET.get('page', 1)
-    paginator = Paginator(ticket_item, 10)
+def view_tickets(request, key=None):
+    q = Q(user=request.user) & Q(deleted=False)
+    get_query = request.GET.get('find')
 
-    try:
-        tickets = paginator.page(page)
-    except PageNotAnInteger:
-        tickets = paginator.page(1)
-    except EmptyPage:
-        tickets = paginator.page(paginator.num_pages)
+    if get_query:
+        q = q & Q(title__iregex=get_query)
 
-    context = {
-        'tickets': tickets,
-        'tickets_quantity': tickets_quantity,
-        'filter_by': FILTER_BY,
-        'title': 'Tickets',
-    }
-    return render(request, 'accounting/index.html', context=context)
-
-
-@login_required
-def view_filtered_tickets(request, key=None):
-    q = Q(user=request.user)
-    if key == 'waiting':
+    if key == 'profit_waiting':
         q = q & Q(profit=None)
 
-    if key == 'failure':
+    if key == 'profit_failure':
         q = q & Q(profit__lt=0)
 
-    if key == 'nothing':
+    if key == 'profit_nothing':
         q = q & Q(profit=0)
 
-    if key == 'success':
+    if key == 'profit_success':
         q = q & Q(profit__gt=0)
 
-    ticket_item = Ticket.objects.filter(q)
+    tickets = Ticket.objects.filter(q)
 
     if key == 'bought_highest_to_lowest':
-        ticket_item = ticket_item.order_by('-bought').values()
+        tickets = tickets.order_by('-bought').values()
 
     if key == 'bought_lowest_to_highest':
-        ticket_item = ticket_item.order_by('bought').values()
+        tickets = tickets.order_by('bought').values()
 
     if key == 'sold_highest_to_lowest':
-        ticket_item = ticket_item.order_by('-sold').values()
+        tickets = tickets.order_by('-sold').values()
 
     if key == 'sold_lowest_to_highest':
-        ticket_item = ticket_item.order_by('sold').values()
+        tickets = tickets.order_by('sold').values()
 
     if key == 'profit_highest_to_lowest':
-        ticket_item = ticket_item.order_by('-profit').values()
+        tickets = tickets.order_by('-profit').values()
 
     if key == 'profit_lowest_to_highest':
-        ticket_item = ticket_item.order_by('profit').values()
+        tickets = tickets.order_by('profit').values()
 
     if key == 'date_oldest':
-        ticket_item = ticket_item.order_by('created_at').values()
+        tickets = tickets.order_by('created_at').values()
 
-    tickets_quantity = ticket_item.count()
+    tickets_quantity = tickets.count()
     page = request.GET.get('page', 1)
-    paginator = Paginator(ticket_item, 10)
+    paginator = Paginator(tickets, 10)
     try:
         tickets = paginator.page(page)
     except PageNotAnInteger:
@@ -113,6 +95,9 @@ def add_ticket(request):
     if form.is_valid():
         ticket = form.save(commit=False)
         ticket.user = request.user
+        if ticket.sold:
+            ticket.profit = round(ticket.sold - ticket.bought, 2)
+            ticket.closed = 'True'
         ticket.save()
         messages.success(request, 'Ticket added successfully')
         return redirect(f'add_ticket')
@@ -127,16 +112,16 @@ def add_ticket(request):
 
 @login_required
 def update_ticket(request, id=None):
-    obj = get_object_or_404(Ticket, id=id, user=request.user)
+    obj = get_object_or_404(Ticket, id=id, user=request.user, deleted=False)
     form = TicketForm(instance=obj)
     if request.method == "POST":
         form = TicketForm(request.POST or None, instance=obj)
         if form.is_valid():
-            form = form.save(commit=False)
-            if form.sold:
-                form.profit = round(form.sold - form.bought, 2)
-                form.closed = 'True'
-            form.save()
+            ticket = form.save(commit=False)
+            if ticket.sold:
+                ticket.profit = round(ticket.sold - ticket.bought, 2)
+                ticket.closed = 'True'
+            ticket.save()
             messages.success(request, 'Ticket successfully changed.')
             return redirect('home')
 
