71a3a0d0c1f259f205f63da2a5299dc39927e157
user <kola89611@gmail.com>
2023-07-11 19:48:13 +0300

    Function to view user data. Create a model when registering a user

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='logout'),
     path('password_change/', password_change, name='password_change'),
-    path('account_profile/', update_user_data, name='account_profile'),
+    path('account_profile/', view_user_data, name='account_profile'),
+    path('account_profile/update/', update_user_data, name='update_profile'),
 ]
