f4170d41c69c0d7f2d03e2826f318556a68bedc0
user <kola89611@gmail.com>
2023-07-14 12:52:24 +0300

    Filter also by 'paginate_by' and 'currency'

--- backend/user/management/commands/command_settings_query.py
+++ backend/user/management/commands/command_settings_query.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+from django.db.models import Q
 
 from ...models import CommandPagination, CommandCurrency
 
@@ -66,19 +67,25 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         for currency_dict in CURRENCY:
             currency = currency_dict.get('currency')
-            if not CommandCurrency.objects.filter(pk=currency_dict.get('pk')):
+            if not CommandCurrency.objects.filter(
+                    pk=currency_dict.get('pk'),
+                    currency=currency_dict.get('currency')
+            ):
                 currency_model = CommandCurrency(**currency_dict)
                 currency_model.save()
-                self.stdout.write(f'Currency "{currency}" added.')
+                self.stdout.write(f'Currency "{currency}" added or changed.')
             else:
                 self.stdout.write(f'Currency "{currency}" already exist.')
 
         for pagination_dict in PAGINATION:
             paginate_by = pagination_dict.get('paginate_by')
-            if not CommandPagination.objects.filter(pk=pagination_dict.get('pk')):
+            if not CommandPagination.objects.filter(
+                    pk=pagination_dict.get('pk'),
+                    paginate_by=pagination_dict.get('paginate_by')
+            ):
                 paginate_by_model = CommandPagination(**pagination_dict)
                 paginate_by_model.save()
-                self.stdout.write(f'Pagination "{paginate_by}" added.')
+                self.stdout.write(f'Pagination "{paginate_by}" added or changed.')
             else:
                 self.stdout.write(f'Pagination "{paginate_by}" already exist.')
 
