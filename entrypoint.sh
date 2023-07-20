c8ad5860f932886c4036b483dfa98c03199fae36
user <kola89611@gmail.com>
2023-07-20 13:22:41 +0300

    Docker final changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-service postgresql start
-
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 python manage.py command_filter_query
 python manage.py command_settings_query
-DJANGO_SUPERUSER_USERNAME=admin DJANGO_SUPERUSER_PASSWORD=admin python manage.py createsuperuser --noinput --email admin@admin.com
+DJANGO_SUPERUSER_USERNAME=$DJANGO_SUPERUSER_USERNAME DJANGO_SUPERUSER_PASSWORD=$DJANGO_SUPERUSER_PASSWORD python manage.py createsuperuser --noinput --email $DJANGO_SUPERUSER_EMAIL
 python manage.py test_command_create_tickets
 
 gunicorn backend.wsgi:application --bind 0.0.0.0:8000
