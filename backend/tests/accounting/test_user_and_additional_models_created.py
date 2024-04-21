7c35565ccef61ee17920ebcd34be6976b896467a
user <kola89611@gmail.com>
2024-04-21 14:22:20 +0300

    Updatet: test_user_and_additional_models_created

--- backend/tests/accounting/test_user_and_additional_models_created.py
+++ backend/tests/accounting/test_user_and_additional_models_created.py
@@ -7,22 +7,22 @@ from user.models import UserSettings, UserProfile
 
 @pytest.mark.django_db
 def test_user_and_additional_models_created(user):
-    user = User.objects.filter(
+    user_object = User.objects.filter(
         username='user',
         email='user@user.com',
     ).first()
     settings_user = UserSettings.objects.filter(
-        user=user.pk
+        user=user_object.pk
     ).first()
     profile_user = UserProfile.objects.filter(
-        user=user.pk
+        user=user_object.pk
     ).first()
-    users = User.objects.all()
+    user_objects = User.objects.all()
     profile_users = UserProfile.objects.all()
 
-    assert user in users
+    assert user_object in user_objects
     assert settings_user.paginate_by.paginate_by == 10
     assert settings_user.currency.currency == '$'
-    assert not settings_user.display_symbol
-    assert settings_user.delete_confirmation
+    assert not settings_user.display_symbol  # False
+    assert settings_user.delete_confirmation  # True
     assert profile_user in profile_users
