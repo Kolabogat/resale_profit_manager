1eec24b03605aeae7aa98e4bbd2ae791ec5ad591
user <kola89611@gmail.com>
2024-05-04 13:22:58 +0300

    Updated: accounting tests

--- backend/accounting/tests.py
+++ backend/accounting/tests.py
@@ -8,7 +8,6 @@ from datetime import datetime
 
 class TestTicketModels(TestCase):
     def setUp(self):
-        self.title = 'Ticket title'
         self.bought = 7.56
         self.sold = 8.53
         self.profit = self.sold - self.bought
@@ -19,15 +18,14 @@ class TestTicketModels(TestCase):
         )
         self.ticket_object = Ticket.objects.create(
             user=self.user_object,
-            title=self.title,
+            title='Ticket title',
             bought=self.bought,
         )
-        self.pk = self.ticket_object.pk
 
     def test_ticket_exists(self):
-        ticket = Ticket.objects.filter(title=self.title).first()
+        ticket = Ticket.objects.filter(title=self.ticket_object.title).first()
         self.assertEqual(ticket.user, self.user_object)
-        self.assertEqual(ticket.title, self.title)
+        self.assertEqual(ticket.title, self.ticket_object.title)
         self.assertEqual(ticket.closed, 'False')
         self.assertEqual(ticket.bought, 7.56)
         self.assertIsNone(ticket.sold)
@@ -36,10 +34,9 @@ class TestTicketModels(TestCase):
         self.assertEqual(ticket.created_at.date(), self.today.date())
         self.assertEqual(ticket.closed_at.date(), self.today.date())
         self.assertEqual(ticket.deleted, 'False')
-        print('SUCCESS | TestAccountingModels | test_ticket_exists')
 
     def test_ticket_update(self):
-        ticket = Ticket.objects.filter(title=self.title).first()
+        ticket = Ticket.objects.filter(title=self.ticket_object.title).first()
         ticket.title = 'Ticket'
         ticket.sold = self.sold
         ticket.profit = self.profit
@@ -47,19 +44,15 @@ class TestTicketModels(TestCase):
         self.assertEqual(ticket.title, 'Ticket')
         self.assertEqual(ticket.sold, self.sold)
         self.assertEqual(ticket.profit, self.profit)
-        print('SUCCESS | TestAccountingModels | test_ticket_update')
 
     def test_get_update_url(self):
-        self.assertEqual(Ticket.get_update_url(self), f'/update/{self.pk}')
-        print('SUCCESS | TestAccountingModels | test_get_update_url')
+        self.assertEqual(Ticket.get_update_url(self.ticket_object), f'/update/{self.ticket_object.pk}')
 
     def test_get_delete_url(self):
-        self.assertEqual(Ticket.get_delete_url(self), f'/delete/{self.pk}')
-        print('SUCCESS | TestAccountingModels | test_get_delete_url')
+        self.assertEqual(Ticket.get_delete_url(self.ticket_object), f'/delete/{self.ticket_object.pk}')
 
     def test_str_method(self):
-        self.assertEqual(Ticket.__str__(self), self.title)
-        print('SUCCESS | TestAccountingModels | test_str_method')
+        self.assertEqual(Ticket.__str__(self.ticket_object), self.ticket_object.title)
 
 
 class TestTicketFilterModel(TestCase):
@@ -76,4 +69,3 @@ class TestTicketFilterModel(TestCase):
             self.assertEqual(ticket_filter.url_value, filter_dict.get('url_value'))
             self.assertEqual(ticket_filter.annotation, filter_dict.get('annotation'))
             self.assertEqual(ticket_filter.color, filter_dict.get('color'))
-        print('SUCCESS | TestTicketFilterModel | test_ticket_filter')
