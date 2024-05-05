16b6400c9c8027b63e42b626e3842b869244e61f
user <kola89611@gmail.com>
2024-05-05 22:37:00 +0300

    Added: description to user/tests

--- backend/user/tests.py
+++ backend/user/tests.py
@@ -16,6 +16,10 @@ class TestUserModels(TestCase):
         )
 
     def test_user_settings(self):
+        """
+        Check that additional model 'UserSettings' for User
+        created correctly
+        """
         user_settings = UserSettings.objects.create(
             user=self.user
         )
@@ -26,6 +30,10 @@ class TestUserModels(TestCase):
         self.assertTrue(user_settings.delete_confirmation)
 
     def test_user_profile(self):
+        """
+        Check that additional model 'UserProfile' for User
+        created correctly
+        """
         user_profile = UserProfile.objects.create(
             user=self.user
         )
@@ -38,6 +46,10 @@ class TestUserModels(TestCase):
 
 class TestUserViews(TestCase):
     def setUp(self):
+        """
+        Setup: TicketFilter, Client, User, UserProfile,
+        UserSettings, Ticket, login user, urls.
+        """
         for filter_dict in FILTER_TICKETS:
             filter_model = TicketFilter(**filter_dict)
             filter_model.save()
@@ -70,6 +82,12 @@ class TestUserViews(TestCase):
         self.user_settings = reverse('user_settings')
 
     def test_register_POST(self):
+        """
+        Test register view
+        Create: register new user
+        Check: additional models for user created
+        correctly; status code; used template.
+        """
         self.client.logout()
         response = self.client.post(self.register, data={
             'username': 'test_user_2',
@@ -89,10 +107,22 @@ class TestUserViews(TestCase):
         self.assertEqual(profile_user.all_time_profit, 0)
 
     def test_register_not_allowed_for_auth_user_GET(self):
+        """
+        Test register view
+        Check: registration template,
+        not available for logged-in user,
+        instead user redirected to home page;
+        status code.
+        """
         response = self.client.get(self.register)
         self.assertEqual(response.status_code, 302)
 
     def test_login_POST(self):
+        """
+        Test login view
+        Check: login work correctly;
+        status code; used template.
+        """
         self.client.logout()
         response = self.client.post(self.login, data={
             'username': 'test_user',
@@ -102,16 +132,33 @@ class TestUserViews(TestCase):
         self.assertTemplateUsed('user/login.html')
 
     def test_login_not_allowed_for_auth_user_GET(self):
+        """
+        Test login view
+        Check: login template,
+        not available for logged-in user,
+        instead user redirected to home page;
+        status code.
+        """
         response = self.client.get(self.login)
         self.assertEqual(response.status_code, 302)
 
     def test_logout_GET(self):
+        """
+        Test logout view
+        Check: user logout and redirected
+        to login page; status code; redirect
+        """
         response = self.client.get(self.logout, follow=True)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.redirect_chain[-1][-1], 302)
         self.assertEqual(response.resolver_match.url_name, 'login')
 
     def test_password_change_success_POST(self):
+        """
+        Test password_change view
+        Check: password changed correctly;
+        status code; used template
+        """
         response = self.client.post(self.password_change, data={
             'new_password1': 'strong123pass974H',
             'new_password2': 'strong123pass974H',
@@ -122,6 +169,11 @@ class TestUserViews(TestCase):
         self.assertTemplateUsed('user/password_change.html')
 
     def test_password_change_error_short_password_POST(self):
+        """
+        Test password_change view
+        Check: error "password is too short" appeared;
+        status code
+        """
         response = self.client.post(self.password_change, follow=True, data={
             'new_password1': '123',
             'new_password2': '123',
@@ -132,6 +184,11 @@ class TestUserViews(TestCase):
         self.assertEqual(response.resolver_match.url_name, 'password_change')
 
     def test_password_change_error_no_match_POST(self):
+        """
+        Test password_change view
+        Check: that error "fields didn’t match" appeared;
+        status code
+        """
         second_response = self.client.post(self.password_change, follow=True, data={
             'new_password1': '1342154',
             'new_password2': '123',
@@ -139,23 +196,31 @@ class TestUserViews(TestCase):
         self.assertContains(second_response, 'The two password fields didn’t match.')
 
     def test_password_change_error_one_field_POST(self):
+        """
+        Test password_change view
+        Check: that error "field is required" appeared;
+        status code
+        """
         third_response = self.client.post(self.password_change, follow=True, data={
             'new_password1': '1342154',
         })
         self.assertContains(third_response, 'This field is required.')
 
     def test_account_profile_GET(self):
+        """
+        Test view_user_data view
+        Check: status code; used template
+        """
         response = self.client.get(self.account_profile)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'user/account_profile.html')
 
     def test_update_profile_GET(self):
-        user_profile = UserProfile.objects.filter(user=self.user.pk).first()
-        self.assertEqual(user_profile.highest_profit, 0)
-        self.assertEqual(user_profile.highest_loss, 0)
-        self.assertEqual(user_profile.all_time_profit, 0)
-        self.assertEqual(user_profile.tickets_quantity, 0)
-
+        """
+        Test update_user_data view
+        Check: profile updated correctly;
+        status code; redirect
+        """
         response = self.client.get(self.update_profile, follow=True)
 
         user_profile = UserProfile.objects.filter(user=self.user.pk).first()
@@ -168,6 +233,11 @@ class TestUserViews(TestCase):
         self.assertEqual(response.resolver_match.url_name, 'account_profile')
 
     def test_update_profile_with_two_tickets_GET(self):
+        """
+        Test update_user_data view
+        Check: profile updated correctly;
+        status code; redirect
+        """
         Ticket.objects.create(
             user=self.user,
             title='Ticket title',
@@ -183,6 +253,10 @@ class TestUserViews(TestCase):
         self.assertEqual(user_profile.tickets_quantity, 2)
 
     def test_user_settings_GET(self):
+        """
+        Test update_user_data view
+        Check: status code; used template
+        """
         response = self.client.get(self.user_settings)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'user/user_settings.html')
