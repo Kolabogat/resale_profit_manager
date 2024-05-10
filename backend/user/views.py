abc693a1f7cfbe67df46a13a542da1994a73f186
user <kola89611@gmail.com>
2024-05-10 18:50:44 +0300

    Deleted: try/except in update_user_data view

--- backend/user/views.py
+++ backend/user/views.py
@@ -128,23 +128,22 @@ def update_user_data(request):
     Updates 'UserProfile' data: profit for all time, tickets quantity,
     highest profit, highest loss.
     """
-    try:
-        q = Q(user=request.user) & Q(deleted=False)
-        tickets = Ticket.objects.filter(q)
-        user_object = get_object_or_404(UserProfile, user=request.user)
+    q = Q(user=request.user) & Q(deleted=False)
+    tickets = Ticket.objects.filter(q)
+    user_object = get_object_or_404(UserProfile, user=request.user)
 
-        if tickets:
-            user_object.all_time_profit = round(tickets.aggregate(Sum('profit')).get('profit__sum'), 2)
-            user_object.tickets_quantity = tickets.count()
-            highest_profit = round(tickets.aggregate(Max('profit')).get('profit__max'), 2)
-            if highest_profit >= 0:
-                user_object.highest_profit = highest_profit
-            highest_loss = round(tickets.aggregate(Min('profit')).get('profit__min'), 2)
-            if highest_loss <= 0:
-                user_object.highest_loss = highest_loss
-            user_object.save()
-            messages.success(request, 'You successfully updated your data.')
-    except Exception:
+    if tickets:
+        user_object.all_time_profit = round(tickets.aggregate(Sum('profit')).get('profit__sum'), 2)
+        user_object.tickets_quantity = tickets.count()
+        highest_profit = round(tickets.aggregate(Max('profit')).get('profit__max'), 2)
+        if highest_profit >= 0:
+            user_object.highest_profit = highest_profit
+        highest_loss = round(tickets.aggregate(Min('profit')).get('profit__min'), 2)
+        if highest_loss <= 0:
+            user_object.highest_loss = highest_loss
+        user_object.save()
+        messages.success(request, 'You successfully updated your data.')
+    else:
         messages.error(request, 'You don\'t have any tickets.')
     return redirect('account_profile')
 
