5a807d3d2e64d19c16d4306e0fc45e15af2771af
user <kola89611@gmail.com>
2024-05-11 17:26:30 +0300

    Added: tests for delete_ticket

---
+++ backend/tests/accounting/test_view_delete_ticket.py
@@ -0,0 +1,38 @@
+import pytest
+
+from django.contrib.messages import get_messages
+from django.urls import reverse
+
+from accounting.models import Ticket
+from tests.conftest import created_user, client
+
+
+@pytest.mark.django_db
+def test_delete_ticket(created_user, register_user, client, ticket):
+    delete_ticket_endpoint = reverse('delete_ticket', args=[ticket.pk])
+    response = client.get(delete_ticket_endpoint)
+    deleted_ticket_exists = Ticket.objects.filter(
+        title=ticket.title,
+        bought=ticket.bought,
+        deleted=True,
+    ).exists()
+    alert_message = f'Ticket "{ticket.title}" successfully deleted.'
+    response_message = str(list(get_messages(response.wsgi_request))[0])
+
+    assert response.status_code == 302
+    assert deleted_ticket_exists
+    assert response.url == reverse('home')
+    assert alert_message in response_message
+
+
+@pytest.mark.django_db
+def test_delete_ticket_not(created_user, register_user, client):
+    ticket_pk = 2345756
+    delete_ticket_endpoint = reverse('delete_ticket', args=[ticket_pk])
+    response = client.get(delete_ticket_endpoint)
+    alert_message = f'Ticket with ID: "{ticket_pk}" not found.'
+    response_message = str(list(get_messages(response.wsgi_request))[0])
+
+    assert response.status_code == 302
+    assert response.url == reverse('home')
+    assert alert_message in response_message
