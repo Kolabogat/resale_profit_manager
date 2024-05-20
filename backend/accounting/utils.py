d99932f468f0557f446bcda7224c05209f02a751
user <kola89611@gmail.com>
2024-05-20 18:20:04 +0300

    Updated: filter functions

--- backend/accounting/utils.py
+++ backend/accounting/utils.py
@@ -1,41 +1,41 @@
 from django.db.models import Q
 
 from accounting.models import Ticket
+from backend.const import TICKETS_FILTER_STATE, TICKET_FILTERS_ORDER_BY
 
 
-def tickets_filter(request, ticket_filter_query, search_filter, filter_by):
+def tickets_filter(request, search_filter, filter_by):
     """
-    Accepts values: request, ticket_query, search_filter, filter_by.
-    Where ticket_filter_query - all filters from DB,
-    search_filter - search word, filter_by - specific filter world.
+    Accepts values: request, search_filter, filter_by.
+    Where  search_filter - search word,
+    filter_by - specific filter word.
 
     Function at first checks if 'search_filter' exists
-    and then checks if there is a 'filter_by' filter exist in database.
+    and then checks if there is a 'filter_by' filter exist in constant.
     """
     q = Q(user=request.user) & Q(deleted=False)
 
     if search_filter:
         q = q & Q(title__iregex=search_filter)
 
-    if filter_by == ticket_filter_query.get(pk=1).url_value:
-        q = q & Q(profit=None)
-
-    if filter_by == ticket_filter_query.get(pk=2).url_value:
-        q = q & Q(profit__lt=0)
-
-    if filter_by == ticket_filter_query.get(pk=3).url_value:
-        q = q & Q(profit=0)
-
-    if filter_by == ticket_filter_query.get(pk=4).url_value:
-        q = q & Q(profit__gt=0)
+    for ticket_filter in TICKETS_FILTER_STATE:
+        filter_url = ticket_filter.get('url_value')
+        if filter_by == filter_url:
+            if filter_url == 'profit_waiting':
+                q = q & Q(profit=None)
+            if filter_url == 'profit_failure':
+                q = q & Q(profit__lt=0)
+            if filter_url == 'profit_nothing':
+                q = q & Q(profit=0)
+            if filter_url == 'profit_success':
+                q = q & Q(profit__gt=0)
 
     tickets = Ticket.objects.filter(q)
 
-    for id_number in range(5, 12):
-        filter_value = ticket_filter_query.get(pk=id_number)
-        if filter_by == filter_value.url_value:
-            if filter_value.title != 'Date':
+    for ticket_filter in TICKET_FILTERS_ORDER_BY:
+        if filter_by == ticket_filter.get('url_value'):
+            if 'sold' in filter_by or 'profit' in filter_by:
                 tickets = tickets.filter(~Q(profit=None))
-            tickets = tickets.order_by(filter_value.query_value).values()
+            tickets = tickets.order_by(ticket_filter.get('query_value')).values()
 
     return tickets
