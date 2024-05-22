0eae24be7888cac48b640b120b1672382d4b1a16
user <kola89611@gmail.com>
2024-05-22 15:08:51 +0300

    Added: deploy settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+# ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
