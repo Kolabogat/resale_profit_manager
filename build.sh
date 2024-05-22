7b2866ebdebdd9a36991eb1070063590a06dd270
user <kola89611@gmail.com>
2024-05-22 16:12:38 +0300

    Try to deploy

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+mkdir ~/.npm-global
+export NPM_CONFIG_PREFIX=~/.npm-global
+export PATH=$PATH:~/.npm-global/bin
+echo -e "export NPM_CONFIG_PREFIX=~/.npm-global\nexport PATH=$PATH:~/.npm-global/bin" >> ~/.bashrc
+
 pyhton -m pip install -r requirements.txt
 
 python manage.py makemigrations --no-input
