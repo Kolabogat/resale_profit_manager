e1c03ab969724a81e1c7b6dd01c1ee663f00d6f1
user <kola89611@gmail.com>
2024-05-22 15:49:08 +0300

    Added: deploy settings

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+pyhton3.10 -m pip install -r requirements.txt
+
+python manage.py makemigrations --no-input
+python manage.py migrate --no-input
+
+python manage.py collectstatic --no-input
+
+python manage.py command_settings_query
