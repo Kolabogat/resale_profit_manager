7cfd1536d55fa8e04f0ca6c40b89089206042e5f
user <kola89611@gmail.com>
2024-05-22 16:55:01 +0300

    Try to deploy

--- backend/build.sh
+++ backend/build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 echo "Building the project..."
-python3.9 -m pip install -r requirements.txt
+python3.10 -m pip install -r requirements.txt
 
 echo "Make Migration..."
-python3.9 manage.py makemigrations --no-input
-python3.9 manage.py migrate --no-input
+python3.10 manage.py makemigrations --no-input
+python3.10 manage.py migrate --no-input
 
 echo "Collect Static..."
-python3.9 manage.py collectstatic --no-input
+python3.10 manage.py collectstatic --no-input
 
 echo "Add Settings to Model..."
-python3.9 manage.py command_settings_query
+python3.10 manage.py command_settings_query
